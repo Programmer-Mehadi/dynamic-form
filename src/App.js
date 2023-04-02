df2af6ac0b13abbe30bd6cceb00e94a84423d53b
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-04-02 14:08:23 +0600

    redux setup and redux value to UI done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const fieldList = useSelector((state) => state.fieldList);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-4 md:p-8 bg-sky-950">
+      <h2 className="text-[35px] font-bold text-slate-950 bg-slate-400 border-2 border-slate-600 rounded-[8px] p-2 md:p-8 py-1 md:py-2 text-center max-w-[600px] mx-auto my-6 shadow-sm uppercase">
+        Dynamic Form
+      </h2>
+      <form
+        className="w-full max-w-[800px] mx-auto bg-slate-300 p-4 md:p-16 rounded-[8px]"
+        onSubmit={handleSubmit}
+      >
+        {/* dynamic form */}
+        <div className="grid gap-4">
+          {fieldList?.map((field, index) => {
+            const {
+              label,
+              inputType,
+              placeholder,
+              child,
+              fieldName,
+              value,
+              min,
+              max,
+            } = field;
+            if (
+              inputType === "text" ||
+              inputType === "number" ||
+              inputType === "range"
+            ) {
+              return (
+                <div className="child">
+                  <label className="text-xl mb-2 block font-semibold">
+                    {label}
+                  </label>
+                  <input
+                    name={fieldName}
+                    type={inputType}
+                    className={
+                      inputType === "range"
+                        ? inputType
+                        : "w-full rounded-[8px] text-base input input-bordered border-1 border-slate-900  p-4"
+                    }
+                    placeholder={placeholder}
+                    defaultValue={value}
+                    min={min ? min : 0}
+                    max={max ? max : 0}
+                    onChange={(e) => {
+                      dispatch({
+                        type: "SET_VALUE",
+                        payload: {
+                          value: e.target.value,
+                          path: {
+                            first: fieldName,
+                            second: "",
+                          },
+                        },
+                      });
+                    }}
+                  />
+                </div>
+              );
+            } else if (inputType === "checkbox" || inputType === "radio") {
+              return (
+                <div className="child grid gap-2">
+                  <label className="text-xl mb-2 block font-semibold">
+                    {label}
+                  </label>
+                  {child.map((c) => {
+                    return (
+                      <div className="flex gap-2 items-center">
+                        <input
+                          name={fieldName}
+                          type={inputType}
+                          className={inputType + " bg-white"}
+                          onChange={(e) => {
+                            dispatch({
+                              type: "SET_VALUE",
+                              payload: {
+                                value:
+                                  inputType === "checkbox"
+                                    ? e.target.checked
+                                    : true,
+                                path: {
+                                  first: fieldName,
+                                  second: c.fieldName,
+                                },
+                              },
+                            });
+                          }}
+                          value={c.fieldName}
+                          defaultChecked={c.value}
+                        />
+                        <label className="text-xl inline-block font-semibold">
+                          {c?.placeholder}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            } else if (inputType === "textarea") {
+              return (
+                <div className="child grid gap-2">
+                  <label className="text-xl mb-2 block font-semibold">
+                    {label}
+                  </label>
+                  <div className="flex">
+                    <textarea
+                      name={fieldName}
+                      type={inputType}
+                      className={inputType + " w-full bg-white"}
+                      onChange={(e) => {
+                        dispatch({
+                          type: "SET_VALUE",
+                          payload: {
+                            value: e.target.value,
+                            path: { first: fieldName, second: "" },
+                          },
+                        });
+                      }}
+                      defaultValue={value}
+                    />
+                  </div>
+                </div>
+              );
+            } else if (inputType === "select") {
+              return (
+                <div>
+                  <label className="text-xl mb-2 block font-semibold">
+                    {label}
+                  </label>
+                  <select
+                    className="select select-bordered w-full"
+                    onChange={(e) => {
+                      dispatch({
+                        type: "SET_VALUE",
+                        payload: {
+                          value: e.target.value,
+                          path: {
+                            first: fieldName,
+                            second: e.target.value,
+                          },
+                        },
+                      });
+                    }}
+                  >
+                    {child.map((c) => {
+                      return <option value={c.value}>{c.title}</option>;
+                    })}
+                  </select>
+                </div>
+              );
+            }
+          })}
+        </div>
+        {/* btn list */}
+        <div className="my-6 mt-10 flex flex-wrap gap-6">
+          <input
+            type="submit"
+            className="btn btn-sm btn-success"
+            value={"Submit"}
+          />
+          <input className="btn btn-sm btn-info" value={"Add new field"} />
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/redux.js
@@ -0,0 +1,152 @@
+import { createStore } from "redux";
+
+const initialstate = {
+  fieldList: [
+    {
+      id: "53535",
+      fieldName: "firstName",
+      inputType: "text",
+      placeholder: "Enter first name",
+      label: "First Name",
+      value: "",
+    },
+    {
+      id: "53535",
+      fieldName: "lastName",
+      inputType: "text",
+      placeholder: "Enter last name",
+      label: "Last Name",
+      value: "",
+    },
+    {
+      id: "53535",
+      fieldName: "skillSets",
+      inputType: "checkbox",
+      label: "Select your skill sets:",
+      child: [
+        {
+          placeholder: "HTML",
+          fieldName: "html",
+          value: false,
+        },
+        {
+          placeholder: "CSS",
+          value: false,
+          fieldName: "css",
+        },
+        {
+          placeholder: "JAVASCRIPT",
+          value: false,
+          fieldName: "javascript",
+        },
+        {
+          placeholder: "REACT",
+          value: false,
+          fieldName: "react",
+        },
+      ],
+    },
+    {
+      id: "53535",
+      fieldName: "gender",
+      inputType: "radio",
+      label: "Select your Gender:",
+      child: [
+        {
+          placeholder: "MALE",
+          fieldName: "male",
+          value: false,
+        },
+        {
+          placeholder: "FEMALE",
+          value: false,
+          fieldName: "female",
+        },
+      ],
+    },
+    {
+      id: "53535",
+      fieldName: "range",
+      inputType: "range",
+      label: "Select your range:",
+      min: 0,
+      max: 1000,
+      value: "850",
+    },
+    {
+      id: "53535",
+      fieldName: "address",
+      inputType: "textarea",
+      label: "Address:",
+      value: "",
+    },
+    {
+      id: "53535",
+      fieldName: "role",
+      inputType: "select",
+      label: "Select role:",
+      value: "",
+      child: [
+        {
+          fieldName: "admin",
+          title: "Admin",
+          value: "admin",
+        },
+        {
+          fieldName: "buyer",
+          title: "Buyer",
+          value: "buyer",
+        },
+        {
+          fieldName: "seller",
+          title: "Seller",
+          value: "seller",
+        },
+      ],
+    },
+  ],
+};
+
+const reducer = (state = initialstate, action) => {
+  switch (action.type) {
+    case "ADD_FIELD":
+      return state;
+    case "SET_VALUE":
+      for (let i = 0; i < state.fieldList.length; i++) {
+        if (state.fieldList[i].fieldName === action.payload.path.first) {
+          if (action.payload.path.second !== "") {
+            for (let j = 0; j < state.fieldList[i].child.length; j++) {
+              if (
+                state.fieldList[i].child[j].fieldName ===
+                action.payload.path.second
+              ) {
+                if (state.fieldList[i].inputType === "select") {
+                  state.fieldList[i].child[j].select = true;
+                } else {
+                  state.fieldList[i].child[j].value = action.payload.value;
+                }
+              } else {
+                if (state.fieldList[i].inputType === "radio") {
+                  state.fieldList[i].child[j].value = false;
+                } else if (state.fieldList[i].inputType === "select") {
+                  state.fieldList[i].child[j].select = false;
+                }
+              }
+            }
+          } else {
+            state.fieldList[i].value = action.payload.value;
+          }
+        }
+      }
+      return state;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
